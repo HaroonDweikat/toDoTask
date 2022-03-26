5b80756746efdc671e7610423ed141414b096f05
HaroonDweikat <ha1999roon@gmail.com>
2022-03-26 23:22:51 +0300

    todo js add, remove done

--- js/toDo.js
+++ js/toDo.js
@@ -23,37 +23,105 @@
 
 // localStorage.setItem('toDos', JSON.stringify(toDos));
 
-const checked = "fa-solid fa-check-square";
-const unChecked = "fa-solid fa-square";
-const liClasses ="todo d-flex justify-content-start align-items-center";
-const contentClasses ="content";
-const toDoUl = document.getElementById('ToDos');
+const checked = "fa-solid fa-circle-check";
+const unChecked = "fa-solid fa-circle";
+const liClasses = "todo d-flex justify-content-start align-items-center";
+const contentClasses = "content";
+const deleteBoxClasses = "ms-auto me-2";
+const deleteIconClasses = "fa-solid fa-trash-can";
+const toDoCompletedUl = document.getElementById('completed');
+const toDoUnCompletedUl = document.getElementById('unCompleted');
 let toDosJson = localStorage.getItem('toDos');
 let toDoList = JSON.parse(toDosJson);
 
+toDoList.sort(compare);
 
-
-toDoList.forEach(todo => {
-    let todoElem = document.createElement('li');
-    let icon = document.createElement('i');
-    let content = document.createElement('div');
-
-    //classes
-    todoElem.className = liClasses;
-    if (todo.completed) {
-        icon.className = checked;
+let newTodoContent = document.getElementById('newTodoContent');
+console.log(toDoList);
+function addNewTodo() {
+    if (newTodoContent.value == '') {
+        console.log('Empty');
+        return;
     }
-    else {
-        icon.className = unChecked;
-    }
-    content.className = contentClasses;
-
-    // content 
-    content.textContent = todo.content;
-    // add icon and content to li
-    todoElem.appendChild(icon);
-    todoElem.appendChild(content);
     
-    //add todo to html
-    toDoUl.appendChild(todoElem);
-});
+    toDoList.push({
+        content: newTodoContent.value,
+        completed: false
+    });
+
+    reBuildList();
+    newTodoContent.value ='';
+}
+function compare(a, b) {
+    if (a.completed) {
+        return 1;
+    }
+    if (a.completed === b.completed) {
+        return 0;
+    }
+    return -1;
+}
+
+document.addEventListener('onLoad', reBuildList());
+
+
+
+function reBuildList() {
+    toDoList.sort(compare);
+    toDoCompletedUl.innerHTML = '';
+    toDoUnCompletedUl.innerHTML = '';
+    for (let index = 0; index < toDoList.length; index++) {
+        let todoElem = document.createElement('li');
+        let iconBox = document.createElement('div');
+        let icon = document.createElement('i');
+        let deleteIcon = document.createElement('i');
+
+        let content = document.createElement('div');
+        let deleteBox = document.createElement('div');
+        //classes
+        todoElem.className = liClasses;
+        icon.className = toDoList[index].completed ? checked : unChecked;
+        content.className = contentClasses;
+        deleteIcon.className =  deleteIconClasses;
+        deleteBox.className =  deleteBoxClasses;
+        //events
+        deleteBox.addEventListener("click", function(event){
+            console.log(toDoList);
+            toDoList.splice(index,1);
+            console.log(toDoList);
+            reBuildList();
+        });
+        iconBox.addEventListener("click", function (event) {
+            toDoList[index].completed = !toDoList[index].completed;
+           
+
+            if (toDoList[index].completed) {
+                //completed 
+                iconBox.firstChild.classList.toggle('fa-circle-check');
+                reBuildList();
+            } else {
+                //unCompleted 
+                iconBox.firstChild.classList.toggle('fa-circle');
+                reBuildList();
+            }
+
+        });
+
+        // content 
+        content.textContent = toDoList[index].content;
+        // add icon and content to li
+        iconBox.appendChild(icon)
+        deleteBox.appendChild(deleteIcon)
+        todoElem.appendChild(iconBox);
+        todoElem.appendChild(content);
+        todoElem.appendChild(deleteBox);
+        //add todo to html
+        if (toDoList[index].completed) {
+            toDoCompletedUl.appendChild(todoElem);
+        } else {
+            toDoUnCompletedUl.appendChild(todoElem);
+        }
+
+    };
+    localStorage.setItem('toDos', JSON.stringify(toDoList));
+}
